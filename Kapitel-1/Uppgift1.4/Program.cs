867a99dc4bf759a605b3a3fcf0f6679d6010a0c0
Adrian HerreraAlgotsson <blazer1458@gmail.com>
2021-09-15 10:08:25 +0200

    Organiserat kapitlen

---
+++ Kapitel-1/Uppgift1.4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
