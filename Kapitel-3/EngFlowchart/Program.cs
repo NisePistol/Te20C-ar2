35f5ff7becb5cbe83a385571cfbb64367153338d
Adrian HerreraAlgotsson <blazer1458@gmail.com>
2021-09-29 09:59:19 +0200

    Fortsättning på interaktiv berättelse

---
+++ Kapitel-3/EngFlowchart/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System;
+
+namespace EngFlowchart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Clear();
+
+            Console.WriteLine("Engeneering flowchart");
+            
+            Console.Write("Does it move? (y/n)");
+            string answer1 = Console.ReadLine().ToLower();
+
+            if (answer1=="y") {
+                Console.Write("Should it? (y/n) ");
+                string answer2 = Console.ReadLine().ToLower();
+
+                if (answer2=="y") {
+                    Console.WriteLine("No problem! 😀");
+                }
+                else if (answer2 == "n") {
+                    Console.WriteLine("Use duck tape! 😆");
+                }
+            }
+            else if (answer1=="n") {
+                Console.Write("Should it? (y/n) ");
+                string answer3 = Console.ReadLine().ToLower();
+
+                if (answer3=="y") {
+                    Console.Write("Use WD-40 😉");    
+                }
+                else if (answer3=="n") {
+                    Console.Write("No problem! 🤣");
+                }
+            }
+            
+        }
+    }
+}
--- Kapitel-3/Interaktiv-berättelse/Program.cs
+++ Kapitel-3/Interaktiv-berättelse/Program.cs
@@ -19,17 +19,18 @@ namespace Interaktiv_berättelse
                 Console.Clear();
 
                 // Skriver ut händelsen och frågar nästa fråga
-                Console.Write("Du väljer att ta bussen till skolan. En man med svart rock kliver på bussen... Han tar fram en pistol och ber dig att ge honom alla dina pengar, ger du honom dina pengar eller försöker du ta pistolen ifrån honom? (svara med \"ge\" eller \"ta\") ");
+                Console.Write("Du väljer att ta bussen till skolan. En man med svart rock kliver på bussen... Han tar fram en pistol och ber dig att ge honom alla dina pengar, ger du honom dina pengar eller försöker du ta pistolen ifrån honom? (ge/ta) ");
                 string rån = Console.ReadLine().ToLower(); // Läser in svaret
 
                 if (rån == "ge") 
                 {
                     // Rensar consolen
                     Console.Clear();
+                    //Ändrar textfärgen till röd
+                    Console.ForegroundColor = ConsoleColor.Red;
 
                     // Skriver ut händelsen
-                    Console.WriteLine("Du ger mannen alla dina pengar och dör sedan av en hjärtattack från all räddsla");
-
+                    Console.WriteLine("Du ger mannen alla dina pengar men dör sedan av en hjärtattack från all räddsla");
                     // Slut på programmet
                 }
                 else if (rån == "ta") 
@@ -38,7 +39,7 @@ namespace Interaktiv_berättelse
                     Console.Clear();
 
                     // Skriver ut händelsen och frågar nästa fråga
-                    Console.Write("Du känner dig modig idag och sträcker ut handen för att ta pistolen ifrån mannen... rånaren hinner inte reagera och du håller nu i pistolen... vill du skjuta rånaren eller springa därifrån? (\"skjuta\" eller \"springa\") ");
+                    Console.Write("Du känner dig modig idag och sträcker ut handen för att ta pistolen ifrån mannen... rånaren hinner inte reagera och du håller nu i pistolen... vill du skjuta rånaren eller springa därifrån? (skjuta/springa) ");
                     string pistol1 = Console.ReadLine().ToLower(); // Läser in svaret
 
                     if (pistol1 == "skjuta") 
@@ -47,23 +48,27 @@ namespace Interaktiv_berättelse
                         Console.Clear();
 
                         // Skriver ut händelsen och frågar nästa fråga
-                        Console.Write("Du skjuter rånaren och han faller ner på marken helt blodig... han är död. Alla på bussen börjar skrika och en gammal tant på bussen tar upp mobilen för att ringa polisen, skjuter du henne också eller springer du därifrån? (skjuta eller springa) ");
+                        Console.Write("Du skjuter rånaren och han faller ner på marken helt blodig... han är död. Alla på bussen börjar skrika och en gammal tant på bussen tar upp mobilen för att ringa polisen, skjuter du henne också eller springer du därifrån? (skjuta/springa) ");
                         string pistol2 = Console.ReadLine().ToLower(); // Läser in svaret
                         
                         if (pistol2 == "skjuta")
                         {
-                            Console.Write("Du skjuter tanten och du ser rädslan i folket runt omkring dig..du springer snabbt av bussen men vet inte vart du ska ta vägen... vart springer du? (höger eller vänster) ");
-                            string håll = Console.ReadLine().ToLower(); // Läser in svaret
-                            
+                            Console.Clear();
+
+                            //Ändrar textfärgen till gul
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write("Du skjuter tanten och du ser rädslan i folket runt omkring dig...du springer snabbt av bussen och går sedan till skolan... Du vann! ");
+                            //Slut på programmet
                         }
                         else if(pistol2 == "springa") 
                         {
                             // Rensar consolen
                             Console.Clear();
+                            //Ändrar textfärgen till röd
+                            Console.ForegroundColor = ConsoleColor.Red;
 
                             // Skriver ut händelsen och frågar nästa fråga
-                            Console.Write("Du struntar i tanten och springer istället av bussen... men halvvägs ut känner du ett pistolskott i ryggen... du faller ihop och vänder dig om och ser tanten stå bredbent och stolt samtidigt som hon ler nöjt mot dig med en pistol i handen... du dog");
-
+                            Console.Write("Du struntar i tanten och springer istället av bussen... men halvvägs ut känner du ett pistolskott i ryggen... du faller ihop och när du vänder dig om så ser du tanten stå bredbent och stolt samtidigt som hon ler nöjt mot dig med en pistol i handen... du dog.");
                             // Slut på programmet
                         }
                     }
@@ -71,10 +76,11 @@ namespace Interaktiv_berättelse
                     {
                         // Rensar consolen
                         Console.Clear();
+                        //Ändrar textfärgen till röd
+                        Console.ForegroundColor = ConsoleColor.Red;
 
                         // Skriver ut händelsen och frågar nästa fråga
                         Console.Write("Du springer av bussen men halvvägs ut känner du ett pistolskott i ryggen... du faller ihop och vänder dig om och ser mannen med en till pistol i handen... du dog");
-
                         // Slut på programmet
                     }
                 }
@@ -86,16 +92,19 @@ namespace Interaktiv_berättelse
                 Console.Clear();
 
                 // Skriver ut händelsen och frågar nästa fråga
-                Console.Write("Du väljer att ta tåget till skolan, men när du kliver av tåget så ramlar du ner på spåret... Nästa tåg kommer om en minut, försöker du klättra upp på perrongen eller gömmer du dig under perrong-skyddet tills nästa tåg har åkt förbi? (svara med \"klättra\" eller \"gömma\"?) ");          
+                Console.Write("Du väljer att ta tåget till skolan, men när du kliver av tåget så ramlar du ner på spåret... Nästa tåg kommer om en minut, försöker du klättra upp på perrongen eller gömmer du dig under perrong-skyddet tills nästa tåg har åkt förbi? (klättra/gömma) ");          
                 string perrong = Console.ReadLine().ToLower(); // Läser in svaret
 
                 if (perrong == "klättra")
                 {   
                     // Rensar consolen
                     Console.Clear();
+                    //Ändrar textfärgen till röd
+                    Console.ForegroundColor = ConsoleColor.Red;
 
                     // Skriver ut händelsen
-                    Console.WriteLine("Du börjar klättra upp på perrongen och en man med svart kappa kommer och hjälper dig upp... men han tar sen din plånbok och knuffar ner dig på spåret igen, en kort stund efter kommer tåget och kör över dig... du dog.");
+                    Console.WriteLine("Du börjar klättra upp på perrongen men du knuffas ner av alla människor som går förbi... en kort stund efter kommer tåget och kör över dig... du dog.");
+                    //Slut på programmet
                 }
                 else if (perrong == "gömma")
                 {   
@@ -103,7 +112,7 @@ namespace Interaktiv_berättelse
                     Console.Clear();
 
                     // Skriver ut händelsen och frågar nästa fråga
-                    Console.Write("Du gömmer dig under Perrongen och väntar ut tåget... Det kommer sedan personal och hjälper dig upp... efter denna traumatiska händelse tappar du suget att gå till skolan och börja överväga att gå hem istället, vart går du? (skolan eller hem?) ");
+                    Console.Write("Du gömmer dig under Perrongen och väntar ut tåget... Det kommer sedan personal och hjälper dig upp... efter denna traumatiska händelse tappar du suget att gå till skolan och börja överväga att gå hem istället, vart går du? (skolan/hem) ");
                     string varthän = Console.ReadLine().ToLower(); // Läser in svaret
                     
                     if(varthän == "hem") 
@@ -112,7 +121,7 @@ namespace Interaktiv_berättelse
                         Console.Clear();
 
                         // Skriver ut händelsen och frågar nästa fråga
-                        Console.WriteLine("Du väljer att gå hem... men påvägen känner du att du börjar bli hungrig så du vill gå och äta nåt, men du har svårt att välja mellan McDonalds och Max... vad väljer du? ");
+                        Console.WriteLine("Du väljer att gå hem... men påvägen känner du att du börjar bli hungrig så du vill gå och äta nåt, vad väljer du? (max/mcdonalds) ");
                         string restaurang = Console.ReadLine().ToLower(); // Läser in svaret
                         
 
@@ -129,20 +138,22 @@ namespace Interaktiv_berättelse
                             {
                                 // Rensar consolen
                                 Console.Clear();
+                                //Ändrar textfärgen till röd
+                                Console.ForegroundColor = ConsoleColor.Red;
 
                                 // Skriver ut händelsen och frågar nästa fråga
                                 Console.Write("Du beställer din kyckling burgare och tycker att den luktar lite konstigt men du tar en tugga ändå... du känner smaken av koriander och tappar direkt all kontroll över din kropp... din sista tanke innan du dör är att du aldrig skulle ha gått till Max... McDonalds e ju faktiskt bättre... Du dog.");
-
                                 // Slut på programmet
                             }
                             else if (burgare == "kött") 
                             {
                                 // Rensar consolen
                                 Console.Clear();
+                                //Ändrar textfärgen till röd
+                                Console.ForegroundColor = ConsoleColor.Red;
 
                                 // Skriver ut händelsen och frågar nästa fråga
-                                Console.Write("Du beställer din kött burgare och tycker att den luktar lite konstigt men du tar en tugga ändå... du känner smaken av gift och tappar direkt all kontroll över din kropp... din sista tanke innan du dör är att du aldrig skulle ha gått till Max... McDonalds e ju faktiskt bättre... Du dog.");
-
+                                Console.Write("Du beställer din kött burgare och tycker att den luktar lite konstigt men du tar en tugga ändå... du känner smaken av koriander och tappar direkt all kontroll över din kropp... din sista tanke innan du dör är att du aldrig skulle ha gått till Max... McDonalds e ju faktiskt bättre... Du dog.");
                                 // Slut på programmet
                             }
                         }
@@ -152,7 +163,7 @@ namespace Interaktiv_berättelse
                             Console.Clear();
 
                             // Skriver ut händelsen och frågar nästa fråga
-                            Console.Write("Du går in på McDonalds för att beställa... tar du McNuggets eller kycklingburgare? (nuggets/kyckling)");
+                            Console.Write("Du går in på McDonalds för att beställa... tar du McNuggets eller kycklingburgare? (mcnuggets/kyckling) ");
                             string donkenMat = Console.ReadLine().ToLower(); // Läser in svaret
                             
                             if (donkenMat == "mcnuggets")
@@ -161,7 +172,30 @@ namespace Interaktiv_berättelse
                                 Console.Clear();
 
                                 // Skriver ut händelsen och frågar nästa fråga
-                                Console.Write("Du får dina nuggets och de smakar jätte gott! På andra sidan av rummet ser du en annan elev som du känner igen... går hen också på din skolan kanske? Stannar du eller går du fram för att prata med personen? (prata/stanna) ");
+                                Console.Write("Du får dina McNuggets och de smakar jätte gott! På andra sidan av rummet ser du en annan elev som du känner igen... Stannar du eller går du fram för att prata med personen? (prata/stanna) ");
+                                string träff1 = Console.ReadLine().ToLower(); // Läser in svaret
+
+                                if (träff1=="stanna")
+                                {   
+                                    // Rensar consolen
+                                    Console.Clear();
+                                    //Ändrar textfärgen till röd
+                                    Console.ForegroundColor = ConsoleColor.Red;
+
+                                    // Skriver ut händelsen och frågar nästa fråga
+                                    Console.WriteLine("Du väljer att stanna... sekunden efter kommer en man med svart rock in för att råna donken... han börjar med att skjuta dig för att få folkets uppmärksamhet... Du dog.");
+                                }
+                                else if (träff1=="prata") 
+                                {   
+                                    // Rensar consolen
+                                    Console.Clear(); 
+                                    //Ändrar textfärgen till gul
+                                    Console.ForegroundColor = ConsoleColor.Yellow; 
+                                    
+                                    // Frågar och tar in första valet från användaren
+                                    Console.WriteLine("Du väljer att gå och prata med personen... när du kommer fram så ser du att det är din bästa kompis... efter det går ni hem och spelar datorspel resten av dagen... Du vann!");
+                                    //Slut på programmet
+                                }
                             }
                             else if (donkenMat == "kyckling")
                             {
@@ -170,9 +204,41 @@ namespace Interaktiv_berättelse
 
                                 // Skriver ut händelsen och frågar nästa fråga
                                 Console.Write("Du får din kycklingburgare och den smakar jätte gott! På andra sidan av rummet ser du en annan elev som du känner igen... Stannar du eller går du fram för att prata med personen? (prata/stanna) ");
-                                string prata = Console.ReadLine(); // Läser in svaret
+                                string träff2 = Console.ReadLine().ToLower(); // Läser in svaret
+
+                                if (träff2=="stanna")
+                                {   
+                                    // Rensar consolen
+                                    Console.Clear();
+                                    //Ändrar textfärgen till röd
+                                    Console.ForegroundColor = ConsoleColor.Red;
+
+                                    // Frågar och tar in första valet från användaren
+                                    Console.WriteLine("Du väljer att stanna... sekunden efter kommer en man med svart rock in för att råna donken han börjar med att skjuta dig för att få folkets uppmärksamhet... Du dog.");
+                                    //Slut på programmet
+                                }
+                                else if (träff2=="prata") 
+                                {   
+                                    // Rensar consolen
+                                    Console.Clear(); 
+                                    //Ändrar textfärgen till gul
+                                    Console.ForegroundColor = ConsoleColor.Yellow; 
+                                    
+                                    // Frågar och tar in första valet från användaren
+                                    Console.WriteLine("Du väljer att gå och prata med personen... när du kommer fram så ser du att det är din bästa kompis... efter det går ni hem och spelar datorspel resten av dagen... Du vann!");
+                                }
                             }
                         }
+                    }
+                    else if(varthän=="skolan")
+                    {
+                        //Rensar consolen
+                        Console.Clear();
+                        //Ändrar textfärgen till röd
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Du väljer att gå till skolan men påvägen så går du på en landmina... du dog");
+                        //slut på programmet
                     }
                 }   
             }                    
