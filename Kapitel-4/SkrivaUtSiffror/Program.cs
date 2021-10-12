7a70161a7c3ac04f1b172df99b4040311112434a
Adrian HerreraAlgotsson <blazer1458@gmail.com>
2021-10-12 10:09:10 +0200

    Skriv ut siffra program

---
+++ Kapitel-4/SkrivaUtSiffror/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SkrivaUtSiffror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal = 50;
+            while(tal!=0) {
+                Console.WriteLine(tal);
+                tal--;
+            }
+        }
+    }
+}
