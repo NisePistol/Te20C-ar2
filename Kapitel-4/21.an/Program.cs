ff1e08a19e9e3332f2074ca86a21536d86eb0978
Adrian HerreraAlgotsson <blazer1458@gmail.com>
2021-10-27 13:20:21 +0200

    GJorde klart Uppgift 3

--- Kapitel-4/21.an/Program.cs
+++ Kapitel-4/21.an/Program.cs
@@ -25,12 +25,16 @@ namespace _21.an
 
                 //slumpar spelarens tal
                 int spelareTal = generator.Next(1, 11);
-                
+
                 //Adderar talet spelaren fick till spelarens summa
                 spelareSumma += spelareTal;
 
                 //Skriver ut
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine($"Du drog en {spelareTal}a");
+
+                //Skriver ut summan
+                Console.WriteLine($"Du har totalt: {spelareSumma}");
 
                 //slumpar datorns tal
                 datorTal = generator.Next(1, 11);
@@ -39,38 +43,53 @@ namespace _21.an
                 datorSumma += datorTal;
 
                 //Skriver ut
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"Datorn drog en {datorTal}a");
 
-                //Skriver ut summorna
-                Console.WriteLine($"Du har totalt: {spelareSumma}");
+                //Skriver ut summan
                 Console.WriteLine($"Datorn har totalt: {datorSumma}");
-
-                if (spelareSumma>=21 || datorSumma>=21) {
+                
+                Console.ForegroundColor = ConsoleColor.White;
+                //Om spelaren går över 21
+                if (spelareSumma >= 21)
+                {
+                    Console.WriteLine("----------\nDu förlora!");
+                    break;
+                } 
+                //Om datorn går över 21
+                else if (datorSumma >= 21)
+                {
+                    Console.WriteLine("----------\nDu vann!");
                     break;
                 }
 
+                //Frågar om spelaren vill dra ett till kort
                 Console.Write("Vill du dra ett till kort? (j/n) ");
                 draKort = Console.ReadLine();
-            }
 
-            Console.Clear();
+                //Om spelaren INTE vill dra ett till kort så ska datorn dra ett sista kort
+                if (draKort != "j")
+                {
+                    //slumpar datorns tal
+                    datorTal = generator.Next(1, 11);
 
-            //slumpar datorns tal
-            datorTal = generator.Next(1, 11);
+                    //Adderar talet datorn fick till datorns summa
+                    datorSumma += datorTal;
 
-            //Adderar talet datorn fick till datorns summa
-            datorSumma += datorTal;
+                    //Skriver ut
+                    Console.WriteLine($"Datorn drog en {datorTal}a och har nu totalt: {datorSumma}\n----------------------------");
 
-            //Skriver ut
-            Console.WriteLine($"Datorn drog en {datorTal}a och har nu totalt: {datorSumma}\n----------------------------");
-
-            if (datorSumma>21 && spelareSumma <= 21)
-            {
-                Console.WriteLine("Du vann");
-            }
-            else
-            {
-                
+                    //Om datorn är över 21 eller om spelaren har mer än datorn
+                    if (datorSumma > 21 || spelareSumma > datorSumma)
+                    {
+                        Console.WriteLine("Du vann!");
+                    }
+                    //Om datorn är under eller likamed 21 och har mer än spelaren
+                    else if (datorSumma > spelareSumma)
+                    {
+                        Console.WriteLine("Du förlora!");
+                    }
+                }
             }
         }
     }
