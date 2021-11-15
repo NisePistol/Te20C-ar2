467d10092dde2794ea4ba7647409c53664f5fd77
Adrian HerreraAlgotsson <blazer1458@gmail.com>
2021-11-15 12:17:53 +0100

    grejer

---
+++ Lek/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string svar = "";
+            while(svar!="1") {
+                Console.WriteLine("skriv 1");
+                svar = Console.ReadLine();
+            }
+        }
+    }
+}
