7fac1b720ff897811f6d2cce4ec10c438509ae08
Adrian HerreraAlgotsson <blazer1458@gmail.com>
2022-01-19 11:08:07 +0100

    Fortsatte med slutscenen och lite mer

--- Raylib/Grunder/Program.cs
+++ Raylib/Grunder/Program.cs
@@ -19,24 +19,29 @@ namespace Grunder
         static int rec2PosX;
         static int rec2PosY;
 
+        static int ballStartPosX;
+        static int ballStartPosY;
+
         static int width = 1000;
         static int height = 600;
-        static string title = "Minecraft";
+        static string title = "Pong";
 
         static int blåPoäng;
         static int rödPoäng;
 
         static int winTxtPosX;
         static int winTxtPosY;
-        static int border = width-500;
+        static int border = width - 500;
+
+        static string spelaIgen = "j";
 
         static void Main(string[] args)
         {
-            int ballStartPosX = width / 2;
-            int ballStartPosY = height / 2;
+            ballStartPosX = width / 2;
+            ballStartPosY = height / 2;
 
-            winTxtPosY = height/9;
-            winTxtPosX = width/2-width/4;
+            winTxtPosY = height / 9;
+            winTxtPosX = width / 2 - width / 4;
 
             ballPosX = ballStartPosX;
             ballPosY = ballStartPosY;
@@ -50,13 +55,11 @@ namespace Grunder
             recPosY = height / 2 - (recH / 2);
             int recSpeed = 10;
 
-            rec2PosX = width / 2 + width / 5;
+            rec2PosX = width - width / 5;
             rec2PosY = recPosY;
 
-            blåPoäng = 5;
+            blåPoäng = 0;
             rödPoäng = 0;
-
-            Texture2D nisse = Raylib.LoadTexture(@"./Resurser/Nisse");
 
             //Starta ett fönster
             Raylib.InitWindow(width, height, title);
@@ -74,7 +77,7 @@ namespace Grunder
 
                 RitaFormer();
 
-                FlyttaSpelare();
+                FlyttaBoll();
 
                 //Om man gör mål
                 if (ballPosX - ballRad > width)
@@ -91,6 +94,8 @@ namespace Grunder
                     ballPosX = ballStartPosX;
                     ballPosY = ballStartPosY;
                     rödPoäng++;
+                    ballSpeedX = 0;
+                    ballSpeedY = 0;
                     ballSpeedX *= -1;
                 }
                 //Bollen studsar på kanterna
@@ -145,10 +150,12 @@ namespace Grunder
                 if (ballPosX - ballRad < recPosX + recW && ballPosY >= recPosY && ballPosY <= recPosY + recH && ballPosX - ballRad! > recPosX)
                 {
                     ballSpeedX *= -1;
+                    ballSpeedY+=1;
                 }
                 else if (ballPosX + ballRad > rec2PosX && ballPosY >= rec2PosY && ballPosY <= rec2PosY + recH && ballPosX + ballRad! < rec2PosX + recW)
                 {
                     ballSpeedX *= -1;
+                    ballSpeedY-=1;
                 }
 
                 if (blåPoäng >= 5 || rödPoäng >= 5)
@@ -161,8 +168,9 @@ namespace Grunder
             }
         }
 
-        static void FlyttaSpelare()
+        static void FlyttaBoll()
         {
+            //Flyttar bollen
             ballPosX += ballSpeedX;
             ballPosY += ballSpeedY;
         }
@@ -179,8 +187,16 @@ namespace Grunder
                 Raylib.DrawRectangle(rec2PosX, rec2PosY, recW, recH, Color.RED);
 
                 //Rita poäng texten
-                Raylib.DrawText($"Poäng: {blåPoäng}", width / 2 - width / 3, 10, width / 27, Color.BLUE);
-                Raylib.DrawText($"Poäng: {rödPoäng}", width / 2 + width / 5, 10, width / 27, Color.RED);
+                Raylib.DrawText($"{blåPoäng}", width / 2 - width / 5, 10, width / 15, Color.BLUE);
+                Raylib.DrawText($"{rödPoäng}", width / 2 + width / 5, 10, width / 15, Color.RED);
+
+                int lineW = 10;
+                Raylib.DrawRectangle(width/2-lineW/2, 10, lineW, height/10, Color.RAYWHITE);
+                Raylib.DrawRectangle(width/2-lineW/2, 110, lineW, height/10, Color.RAYWHITE);
+                Raylib.DrawRectangle(width/2-lineW/2, 210, lineW, height/10, Color.RAYWHITE);
+                Raylib.DrawRectangle(width/2-lineW/2, 310, lineW, height/10, Color.RAYWHITE);
+                Raylib.DrawRectangle(width/2-lineW/2, 410, lineW, height/10, Color.RAYWHITE);
+                Raylib.DrawRectangle(width/2-lineW/2, 510, lineW, height/10, Color.RAYWHITE);
             }
             else
             {
@@ -201,24 +217,71 @@ namespace Grunder
             {
                 winner = "Röd";
             }
-            
+
             if (winTxtPosX < border)
             {
                 winTxtPosX += 5;
-                border = width-500;
+                border = width - 500;
             }
-            else 
+            else
             {
                 winTxtPosX -= 5;
                 border = 100;
             }
-            Raylib.DrawText($"{winner} vann!", winTxtPosX, winTxtPosY, width / 10, Color.PINK);
-            Raylib.DrawText($"Spela igen?", width/3, height/2, width / 15, Color.GOLD);
 
-            int buttonW = width/4;
-            int buttonH = height/6;
-            Raylib.DrawRectangle(width/2-(buttonW/2), height-(buttonH+buttonH/2), buttonW, buttonH, Color.RAYWHITE);
-            Raylib.DrawText("JA", width/2-55, height-(buttonH+buttonH/2)+4, 100, Color.BLACK);
+            //Skriver ut vem som vann
+            Raylib.DrawText($"{winner} vann!", winTxtPosX, winTxtPosY, width / 10, Color.PINK);
+            //Skriver ut "spela igen?"
+            Raylib.DrawText($"Spela igen?", width / 3, height / 2, width / 15, Color.GOLD);
+
+            int buttonW = width / 4;
+            int buttonH = height / 6;
+            int buttonX = width / 2 - (buttonW / 2);
+            int buttonY = height - (buttonH + buttonH / 2);
+
+            int txtX = buttonX + (buttonW / 4) + 5;
+            int txtY = buttonY + (buttonH / 15);
+            int txtSize = 100;
+
+            int mouseX = Raylib.GetMouseX();
+            int mouseY = Raylib.GetMouseY();
+
+            Color buttonColor = Color.RAYWHITE;
+
+            //Om man hooverar över knappen
+            if (mouseX > buttonX && mouseX < buttonX + buttonW && mouseY > buttonY && mouseY < buttonY + buttonH)
+            {
+                //Byter knappens färg
+                buttonColor = Color.LIGHTGRAY;
+
+                //Gör knappen större
+                buttonW += 20;
+                buttonH += 15;
+
+                //Fixar positionen
+                buttonX -= 10;
+                buttonY -= 15 / 2;
+
+                txtSize += 10;
+                txtX -= 5;
+                txtY -= 5;
+            }
+
+            //Om man trycker på börja om knappen, enter eller space
+            if (mouseX > buttonX && mouseX < buttonX + buttonW && mouseY > buttonY && mouseY < buttonY + buttonH && Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON) || Raylib.IsKeyDown(KeyboardKey.KEY_SPACE) || Raylib.IsKeyDown(KeyboardKey.KEY_ENTER))
+            {
+                blåPoäng = 0;
+                rödPoäng = 0;
+                ballSpeedX = 6;
+                ballSpeedY = 6;
+            }
+
+            //Ritar knappen
+            Raylib.DrawRectangle(buttonX, buttonY, buttonW, buttonH, buttonColor);
+            //Ritar knappens kanter
+            Raylib.DrawRectangleLines(buttonX, buttonY, buttonW, buttonH, Color.BLACK);
+            //Ritar texten innuti
+            Raylib.DrawText("JA", txtX, txtY, txtSize, Color.BLACK);
         }
     }
 }
