ee21b7ceefdd8193da3ea460c5c3bd3fa9fbb439
Adrian HerreraAlgotsson <blazer1458@gmail.com>
2022-08-23 09:14:39 +0200

    ia

---
+++ Lek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.CodeDom.Compiler;
+using System;
+
+namespace Lek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random gen = new Random();
+            int[] allaSiffror = new int[10];
+            
+        }
+    }
+}
